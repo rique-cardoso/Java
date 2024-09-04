40cd67eb09e4da8ddbd987d31be408dfaeee6150
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-09-04 12:59:27 -0300

    Polimorfismo de sobrecarga

---
+++ estudos_04-09/polimorfismo/sobrecarga/src/model/Animal.java
@@ -0,0 +1,8 @@
+package model;
+
+public abstract class Animal {
+    protected double peso;
+    protected int idade;
+    protected int membros;
+    public abstract void emitirSom();
+}
---
+++ estudos_04-09/polimorfismo/sobrecarga/src/model/Lobo.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Lobo extends Mamifero{
+    @Override
+    public void emitirSom(){
+        System.out.println("Auuuuuuuuuuuuu!");
+    }
+}
