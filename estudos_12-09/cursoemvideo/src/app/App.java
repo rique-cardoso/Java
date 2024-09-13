d40dd4de121006b9e9664d81c4907e4d4906773b
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-09-13 09:48:33 -0300

    pnúltima aula de poo

---
+++ estudos_12-09/cursoemvideo/src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+import model.*;
+public class App {
+    public static void main(String[] args) {
+        Gafanhoto g1 = new Gafanhoto("Zezé", 14, "M", "zezezinho");
+        System.out.println(g1.toString());
+    }
+}
--- estudos_12-09/cursoemvideo/src/model/Video.java
+++ estudos_12-09/cursoemvideo/src/model/Video.java
@@ -6,19 +6,58 @@ public class Video implements IAcoesVideos{
     private int views;
     private int curtidas;
     private boolean reproduzindo;
+    public Video(String titulo) {
+        this.titulo = titulo;
+        this.avaliacao = 1;
+        this.views = 0;
+        this.curtidas = 0;
+        this.reproduzindo = false;
+    }
     @Override
     public void play() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'play'");
+        this.reproduzindo = true;
     }
     @Override
     public void pause() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pause'");
+        this.reproduzindo = false;
     }
     @Override
     public void like() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'like'");
+        this.curtidas++;
+    }
+    @Override
+    public String toString() {
+        return "Video [titulo=" + titulo + ", avaliacao=" + avaliacao + ", views=" + views + ", curtidas=" + curtidas
+                + ", reproduzindo=" + reproduzindo + "]";
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+    public int getViews() {
+        return views;
+    }
+    public void setViews(int views) {
+        this.views = views;
+    }
+    public int getCurtidas() {
+        return curtidas;
+    }
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+    public boolean isReproduzindo() {
+        return reproduzindo;
+    }
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
     }
 }
