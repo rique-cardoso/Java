faf940aeae7fe20128f4fa72b759572a350ed851
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2026-04-26 00:51:20 -0300

    Criando Classe Contexto

---
+++ padroes_projeto/strategy/ex003/exemplo-pratico/src/CarrinhoDeCompras.java
@@ -0,0 +1,26 @@
+/**
+ * Classe Contexto
+ * é a classe que precisa realizar a ação (no nosso caso, o carrinho de compras). O truque aqui é que o Carrinho não sabe como os pagamentos funcionam. Ele apenas guarda uma referência para a interface EstrategiaPagamento e chama o método pagar()
+ */
+public class CarrinhoDeCompras {
+    private double totalParaPagar;
+    // O Contexto guarda a referência para a Estratégia
+    private EstrategiaPagamento estrategiaPagamento;
+
+    // Construtor
+    public CarrinhoDeCompras(double total){
+        this.totalParaPagar = total;
+    }
+
+    // Permite mudar a estratégia em tempo de execução
+    public void setEstrategiaDePagamento(EstrategiaPagamento estrategia){
+        this.strategiaPagamento = estrategia;
+    }
+    public void finalizarCompra(){
+        if(estrategiaPagamento == null){
+            throw new RuntimeException("Selecione uma forma de pagamento!");
+        }
+        // O carrinho DELEGA o pagamento para a estratégia selecionada
+        estrategiaPagamento.pagar(totalParaPagar);
+    }
+}
